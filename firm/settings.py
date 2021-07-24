effdc708c21956801259042d0f168a9817627fbb
liammukasa <wmukasa@gmail.com>
2021-07-24 13:53:14 +0300

    changed secrete key

--- firm/settings.py
+++ firm/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ht!-4xmk9@_dpcmigec-b_-l+c15)^3j96ya+_#693o^qg-d+^'
-
+#SECRET_KEY = 'django-insecure-ht!-4xmk9@_dpcmigec-b_-l+c15)^3j96ya+_#693o^qg-d+^'
+SECRET_KEY = os.environ.get('django_SECRETE_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

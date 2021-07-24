343295cb78e1886fd8613f60a3255cfd95e79f52
liammukasa <wmukasa@gmail.com>
2021-07-24 11:03:35 +0300

    changed the requirements

--- firm/settings.py
+++ firm/settings.py
@@ -130,9 +130,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-#STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(DIRNAME, 'static')
+#STATIC_ROOT=os.path.join(DIRNAME, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
